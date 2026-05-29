d7d8c8360fc5621b3afeaff7609e701b3b8b1ff5
SasaLax <sasa.lakicbn1@gmail.com>
2026-05-29 13:29:23 +0200

    Add Building, Adress and Location class

---
+++ src/CommunityHub.Application/Domain/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityHub.Application.Domain
+{
+    public class Address
+    {
+        public String Street { get; private set; }
+        public int Number { get; private set; }
+
+        public Address(string street, int number)
+        {
+            this.Street = street;
+            this.Number = number;
+        }
+    }
+}
---
+++ src/CommunityHub.Application/Domain/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityHub.Application.Domain
+{
+    public class Location
+    {
+        public string City { get; private set; }
+
+        public string Country { get; private set; }
+        public Location(string city, string country)
+        {
+            this.City = city;
+            this.Country = country;
+        }
+    }
+}
